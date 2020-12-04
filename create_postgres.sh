4d05bf7ef13aa5047921752cd3e9814e76a6f3f2
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-12-04 16:09:29 +0100

    improve psql script

--- create_postgres.sh
+++ create_postgres.sh
@@ -3,17 +3,18 @@
 echo "choose a workspace for your local db:" 
 read workspace 
 
+
 docker run \
 -d \
---rm \
 -p 5432:5432 \
 -e POSTGRES_HOST_AUTH_METHOD=trust \
 -e POSTGRES_PASSWORD=notneeded \
+-e POSTGRES_DB=${workspace}_db \
 -v psql_$workspace:/var/lib/postgresql/data \
 --name psql_${workspace} \
 postgres
 
 echo "check your psql is connected with the following command:"
-echo "psql -h localhost -p 5432 -U postgres"
+echo "psql -h localhost -p 5432 -U postgres --dbname=${workspace}_db"
 echo "url for apps:"
-echo "psql://postgres@localhost:5432/postgres"
+echo "psql://postgres@localhost:5432/${workspace}_db"
