ee5502d4168f9099c7280b6a556d4bb833e98086
Quentin Gaultier <quentin.gaultier@gmail.com>
2021-01-11 15:49:42 +0100

    enhance postgres tooling

--- create_postgres.sh
+++ create_postgres.sh
@@ -2,22 +2,23 @@
 
 # echo "choose a workspace for your local db:" 
 
+workspace="local"
+
 docker run \
 -d \
 -p 5432:5432 \
 --rm \
 -e POSTGRES_HOST_AUTH_METHOD=trust \
 -e POSTGRES_PASSWORD=notneeded \
--e POSTGRES_DB=${workspace}_db \
 -v psql_$workspace:/var/lib/postgresql/data \
 --name psql_${workspace} \
 postgres
 
 
-workspace=$(openssl rand -hex 4)
+# -e POSTGRES_DB=${workspace}_db \
 
-
-echo export DATABASE_URL="postgres://postgres@localhost:5432/${workspace}_db"
+# workspace=$(openssl rand -hex 4)
+# echo export DATABASE_URL="postgres://postgres@localhost:5432/${workspace}_db"
 
 
 
